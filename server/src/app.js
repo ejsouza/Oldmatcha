9522249b76f1624228aeda3a0a3b85960c98d16c
Eder De Souza <esouza@student.42.fr>
2020-04-24 11:42:12 +0200

    Update app.js
    Created a basic express server and some endpoints for testing

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Bild a very basic express server
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Set ending point
+app.get('/status', (req, res)=> {
+	res.send({
+		message: 'Hello world!'
+	});
+});
+
+app.get('/', (req, res)=> {
+	res.send({
+		message: 'Welcome to the home page!'
+	});
+});
+
+app.listen(process.env.PORT || 8081);
