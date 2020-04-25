e53e82d9b002ab6a129f12cab9cd44272034d79e
Eder De Souza <esouza@student.42.fr>
2020-04-25 09:44:30 +0200

    Update server app.js

--- server/src/app.js
+++ server/src/app.js
@@ -10,16 +10,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Set ending point
-app.get('/status', (req, res)=> {
-	res.send({
-		message: 'Hello world!'
-	});
-});
-
+// GET
 app.get('/', (req, res)=> {
 	res.send({
 		message: 'Welcome to the home page!'
 	});
 });
 
+
+// POST
+app.post('/register', (req, res)=> {
+	res.send({
+		message: `Hello ${req.body.email}! You were registered successfully! Have fun`
+	});
+});
+// The server application is running in port http://localhost:8081 
+// Or it will be running in the env.PORT number if set
 app.listen(process.env.PORT || 8081);
